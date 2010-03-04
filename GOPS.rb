23284d5fb78e3667e785074a3cdd5a57e3ebec53
Sam Warmuth <samwarmuth@sam-warmuths-macbook-pro.local>
2010-03-03 21:22:36 -0600

    looks like everything except straights

--- GOPS.rb
+++ GOPS.rb
@@ -1,1 +1,26 @@
-#GOPS code goes here? Probably an engine that allows players to input their play when certain events happen. This way I can do some work on kent.
+require 'cards'
+
+deck = Deck.new
+players = [Player.new("Player 1"), Player.new("Player 2")]
+
+13.times {deck.draw}
+players.each{|p| p.draw(13,deck)}
+deck.shuffle_remaining
+
+while(!deck.empty?)
+  puts deck.draw
+  turn = []
+  players.each do |p|
+    puts p
+    print "Which card will you use? "
+    card = gets.chomp!
+    card + p.hand.first.suit[0,1] if card.length == 1 #add suit
+    puts card
+    turn << p.discard(card)
+  end
+  puts "Used " +turn.join(" and ") + " this turn"
+  puts "--------"
+end
+
+puts players.join("\n")
+puts deck
--- cards.rb
+++ cards.rb
@@ -1,34 +1,33 @@
 #A deck of cards. Theoretically should be usable by any program that needs cards.
-#Each deck is an array of cards
-#Each card is a two-element array [value, suit]
-#Suits "Hearts", "Spades", "Clubs", "Diamonds"
 
 class Deck
-  attr_reader :deck, :drawn
+  attr_reader :deck, :discard
   def initialize
     @deck = Array.new(52){|i| Card.new(51-i)}
-    @drawn = []
+    @discard = []
   end
   def draw
     card = @deck.pop
-    @drawn << card
+    @discard << card
     card
   end
   def shuffle
-    @deck += @drawn
+    @deck += @discard
     @deck = @deck.sort_by{rand}
   end
   def shuffle_remaining
     @deck = @deck.sort_by{rand}
   end
-  def inspect; self.to_s; end
+  def empty?
+    @deck.empty?
+  end
   def to_s
     @deck.collect{|c| c.to_s}.join(', ')
   end
 end
 
 class Card
-  attr_reader :number, :value 
+  attr_reader :value 
   include Comparable
   Suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
   def initialize(input)
@@ -36,24 +35,32 @@ class Card
     when "Fixnum"
         @suit = input/13
         @value = input%13 + 2
-    when "Array"
     when "String"
+      @suit = Suits.map{|s| s[0,1].downcase}.index(input[1,1].downcase)
+      @value = case input[0,1].downcase
+      when "a"; 14
+      when "k"; 13
+      when "q"; 12
+      when "j"; 11
+      when "t"; 10
+      else; input[0,1].to_i
+      end
     else return nil
     end
   end
   def <=>(other)
-    @value*100+@suit
+    @value <=> other.value #suit *not* taken into account
   end
   def suit
     Suits[@suit]
   end
-  def inspect; self.to_s; end
   def to_s
     pretty_val = case @value
     when 14; "Ace"
     when 13; "King"
     when 12; "Queen"
     when 11; "Jack"
+    when 10; "Ten"
     else; @value.to_s
     end
     pretty_val[0,1] + Suits[@suit][0,1]
@@ -71,24 +78,27 @@ end
 
 class Player
   attr_accessor :hand, :name
-  attr_reader :id, :rank
+  attr_reader :id, :rank, :top_hand
   def initialize(name="Player",cards=[])
     @name = name
     @hand = cards
     @id = (Time.now.to_f % 100000 * 10000).to_i
   end
-  def add_cards(cards)
-    cards = Array(cards) # turn '5' into [5]
+  def add_cards(*cards)
+    cards = Array(cards)
     @hand += cards
-    rank
+    find_rank
   end
   def draw(number, deck)
     number.times{@hand << deck.draw}
+    find_rank
   end
   def discard(card)
     case card.class.to_s
     when "String"
-      @hand.delete_if{|c| c.to_s.downcase == card.downcase}
+      drawn = @hand.select{|c| c.to_s.downcase == card.downcase}
+      @hand.delete(drawn)
+      return drawn
     end
   end
   def sort_by(tag)
@@ -96,43 +106,53 @@ class Player
     #suit, value
   end
   def rank
-    by_value = @hand.group_by{|c|c.value}
-    by_suit = @hand.group_by{|c|c.suit}
+    @rank
+  end
+  def find_rank
+    
+    by_value = @hand.group_by{|c|c.value} # creates hash {2 => [2C, 2D], 3 => [3H, 3S, 3C]...}
+    by_suit = @hand.group_by{|c|c.suit}   # creates hash {"Diamonds" => [2D, 3D, 8D], "Clubs" => [2C]...}
     
     #eventually I should only find the ones that matter
-    pair =  by_value.select{|key,val| val.length == 2}
-    three =  by_value.select{|key,val| val.length == 3}
-    four = by_value.select{|key,val| val.length == 4}
-    flush = by_suit.select{|key,val| val.length >= 5}
-    straight = (2..10).map{|i| by_value.has_keys?((i..i+4)) ? (print "YAY #{i}!") : nil}.compact!
-    puts straight.inspect
-    puts by_value.has_keys?((6..10))
+    pair =  by_value.select{|key,val| val.length == 2}.map{|a,b| b.sort.reverse}.sort_by{|n| n[0].value}.reverse
+    three =  by_value.select{|key,val| val.length == 3}.map{|a,b| b.sort.reverse}.sort_by{|n| n[0].value}.reverse.first
+    four = by_value.select{|key,val| val.length == 4}.map{|a,b| b.sort.reverse}.sort_by{|n| n[0].value}.reverse.first
+    flush = by_suit.select{|key,val| val.length >= 5}.map{|a,b| b.sort.reverse}.sort_by{|n| n[0].value}.reverse.first
+    high_cards = @hand.sort.reverse
     
-    
-    # so, check for flushes & straights
-    # if both, check for straight flush (hard) -- sort flush?
-    # 4 of a kind?
-    # get pairs & threes.
-    # Both? Full House, find the biggest
-    # Find highest triple
-    # Two Pair?
-    # Find highest pair
-    # Find highest card
+    #straight = (2..10).map{|i| by_value.has_keys?((i..i+4)) ? (print "#{i}...#{i+4}!") : nil}.compact!
+    if !four.nil?
+      @top_hand = four
+      @rank = "Four of a Kind"
+    elsif !three.nil? && !pair.empty?
+      @top_hand = three + pair[0]
+      @rank = "Full House"
+    elsif !flush.nil?
+      @top_hand = flush[0,5]
+      @rank = "Flush"
+    elsif !nil.nil?
+      puts " (Straight) Not Implemented."
+    elsif !three.nil?
+      @top_hand = three
+      @rank = "Three of a Kind"
+    elsif pair.length > 1
+      @top_hand = pair[0] + pair[1]
+      @rank = "Two Pair"
+    elsif !pair.first.nil?
+      @top_hand = pair.first
+      @rank = "Pair"
+    else
+      @top_hand = high_cards[0,5]
+      @rank = "Garbage"
+    end
+    while (@top_hand.length < 5 && !high_cards.empty?)
+      c = high_cards.delete_at(0)
+      @top_hand << c if !@top_hand.include?(c)
+    end
+    return @top_hand
 
-
-  end
-  def pretty_score
   end
   def to_s
-    @name + " " + @hand.join(" ")
-    #Player_name: 2S 4H 4D KH 3S (Pair of 4s)
+    @name + ": " + @hand.join(" ")
   end
 end
-
-class Hash
-  def has_keys?(*keys)
-    keys.select{|k,v| !self.include?(k)}.empty?
-  end
-end
-
-m = {:a => 1, :b => 2}
