db61fb7641020981f1932e5d0ff09d72ce13102b
Sam Warmuth <samwarmuth@sam-warmuths-macbook-pro.local>
2010-03-04 22:09:25 -0600

    LOTS of work on eGOPS

--- GOPSlib.rb
+++ GOPSlib.rb
@@ -1,23 +1,41 @@
 require 'cards'
+
 class GOPS
   attr_accessor :deck, :players
-  attr_reader :bid_card
+  attr_reader :bid_card, :current_bids, :id
   def initialize(player1, player2)
+    @id = (Time.now.to_f % 100000 * 10000).to_i
     @deck = Deck.new
     @players = {player1 => "Diamonds", player2 => "Hearts"}
     @hands = {"Diamonds" => Player.new("Diamonds"), "Hearts" => Player.new("Hearts")}
     @played = {"Diamonds" => Player.new("Diamonds"), "Hearts" => Player.new("Hearts")}
     @won = {"Diamonds" => Player.new("Diamonds"), "Hearts" => Player.new("Hearts")}
-    13.times {@deck.draw}
-    @players.each_value{|p| p.draw(13,deck)}
-    @deck.shuffle_remaining
-    @bid_card = @deck.draw
-  end
-  def place_bid(card)
     @current_bids ||= []
+    13.times {@deck.draw}
+    @hands.each_value{|p| p.draw(13,deck)}
+    @deck.shuffle_remaining
+    @bid_card = Array(@deck.draw)
+  end
+  def hand(player)
+    @hands[@players[player]].hand
+  end
+  def played(player)
+    @played[@players[player]].hand
+  end
+  def won(player)
+    @won[@players[player]].hand
+  end
+  def place_bid(player, card)
+    begin
+      card = Card.new(card)
+      raise unless (@players[player] == card.suit)&&(hand(player).include?(card))
+    rescue
+      return false
+    end
     if @current_bids.select{|c| c.suit == card.suit}.empty?
       @current_bids << card 
-      @played[card.suit] << card
+      @hands[card.suit].discard(card)
+      @played[card.suit].hand << card
       score_round if @current_bids.length == @players.length
       return true
     else
@@ -26,47 +44,26 @@ class GOPS
   end
   def score_round
     raise unless @current_bids.length == @players.length
-    winning_card = @current_bids.sort_by{|c| c.value}.reverse.first
-    @current_bids.clear
-    @won[winning_card.suit] << winning_card
-    
-    @bid_card = @deck.draw unless @deck.empty?
+    bids = @current_bids.sort_by{|c| c.value}.reverse
+    if bids[0].value == bids[1].value
+      @current_bids.clear
+      @bid_card << @deck.draw
+    else
+      winning_card = bids.first
+      @current_bids.clear
+      @won[winning_card.suit].hand += @bid_card
+      @bid_card = Array(@deck.draw) unless @deck.empty?
+    end
+  end
+  def already_bid?(player)
+    hand = @played[@players[player]].hand
+    hand = @hands[@players[player]].hand if hand.empty? #one of these will have a card
+    !@current_bids.select{|c| c.suit == hand[0].suit}.empty?
   end
   def player_score(player)
-    @won[@players[player]].inject{|sum,card| sum + card.value}
+    won(player).inject{|sum,card| sum + card.value}
   end
-  def game_finished
+  def game_finished?
     @deck.empty? ? true : false
   end
 end
-
-=begin
-while(!deck.empty?)
-  puts "Bidding On: " + deck.draw.to_s
-  turn = []
-  players.each_value do |p|
-    puts p
-    print "Which card will you use? "
-    puts ""
-    card = gets.chomp!
-    card += p.hand.first.suit[0,1] if card.length == 1 #add suit
-    begin
-      card = Card.new(card)
-      raise unless p.hand.include?(card)
-    rescue
-      redo
-    end
-    turn << card
-    p.discard(card)
-  end
-  winning_card = turn.sort_by{|c| c.value}.reverse.first
-  players[winning_card.suit].score += winning_card.value
-  puts "Used " +turn.join(" and ") + " this turn, " + winning_card.suit + " wins." 
-  puts "--------"
-end
-players.each_value do |p|
-  puts p.name + ": " + p.score.to_s
-end
-winner = players.values.sort_by{|p|p.score}.last
-puts winner.name + " Wins!"
-=end
--- eGOPS.rb
+++ eGOPS.rb
@@ -2,22 +2,71 @@ require 'rubygems'
 require 'sinatra'
 require 'haml'
 require 'base64'
+require 'GOPSlib'
 $players = {}
 $games = {}
+$meet = []
 
 get '/' do
+  @players = $players.values
   haml :welcome
 end
 
-get '/dashboard/:id' do
-  player = players.select{|p| p.id = params[:id]}.first
+post '/' do
+  name = params[:name]
+  player = $players.values.select{|p|p.name == name}.first
+  if player.nil?
+    player = Player.new(name)
+    $players[player.id] = player
+  end
+  redirect "/dashboard/#{player.id}"
 end
 
-get '/game/:game_id' do
-  decoded = Base64.decode64(params[:game_id]).split(" ")
-  game = decoded[0]
-  player = decoded[1]
+get '/dashboard/:id' do
+  @player = $players[params[:id].to_i]
+  if @player
+    games = $games.values
+    @user_games = games.select{|g| g.players.has_key?(@player)}
+  else
+    return "Player not found."
+  end
+  haml :dashboard
 end
+
+
+
+get '/new-game/:player' do
+  player = $players[params[:player].to_i]
+  if $meet.empty?
+    $meet << player.id
+    redirect "/dashboard/#{player.id}"
+  else
+    redirect "/dashboard/#{player.id}" if $meet.include?(player.id)
+    other_player = $players[$meet.pop]
+    game = GOPS.new(player, other_player)
+    $games[game.id] = game
+    redirect "/game/#{game.id}/#{player.id}"
+  end
+end
+
+get '/game/:game_id/:player' do
+  @game = $games[params[:game_id].to_i]
+  @player = $players[params[:player].to_i]
+  @other_player = @game.players.keys.reject{|p| p == @player}.first
+  if @game&&@player&&@other_player
+    haml :game
+  else
+    return "game/player/opponent not found. Sorry."
+  end
+end
+
+get '/game/:game_id/:player/:card' do
+  game = $games[params[:game_id].to_i]
+  player = $players[params[:player].to_i]
+  success = game.place_bid(player, params[:card])
+  redirect "/game/#{game.id}/#{player.id}"
+end
+
 
 
 __END__
@@ -27,6 +76,44 @@ __END__
 
 @@welcome
 %h1 Welcome to Goofspiel
+%form{:method => "POST", :action => "/"}
+  %label Please choose a username:
+  %input{:name => "name", :value => "", :type => "text", :size => "20"}
+%h2 Current Players:
 %p
-  Please choose a username:
-  ________
+  =@players.map{|p| p.name}.join("\n")
+
+@@dashboard
+%h2 Hi there #{@player.name}
+= "You're waiting for someone to play with." if $meet.include?(@player.id)
+-my_games = $games.values.select{|g| g.players.keys.include?(@player)}
+-unless my_games.empty?
+  %h2 Your Games
+  %p
+    -my_games.each do |game|
+      %a{:href => "/game/#{game.id}/#{@player.id}"}
+        Game Against 
+        =game.players.keys.reject{|p| p == @player}.first.name
+      
+%p
+  %a{:href => "/new-game/#{@player.id}"} New Game
+
+@@game
+%h3 #{@player.name}, you're playing against #{@other_player.name}
+%p  Bidding on: #{@game.bid_card.join(" ")}
+%p
+  -if @game.already_bid?(@player)
+    Waiting for #{@other_player.name}'s bid
+    %br
+    Your Cards:
+    %br
+    = @game.hand(@player).join(" ")
+  -else
+    Your Cards:
+    %br
+    -@game.hand(@player).each do |card|
+      %a{:href => "/game/#{@game.id}/#{@player.id}/#{card.to_s}"}= card.to_s
+  -unless @game.won(@player).empty?
+    %p
+      So far, you've won:
+      =@game.won(@player).join(" ") 
